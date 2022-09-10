e2035a716b942a170f8eeedffe19b0031541f45c
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2022-09-10 19:57:41 -0300

    git commit -m "Ajustes"

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './modules/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
   {
     path:'', component:AdminComponent,
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.component').then((Component) => Component.HomeComponent),
   }
+
 ];
 
 @NgModule({
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { HomeComponent } from './modules/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
